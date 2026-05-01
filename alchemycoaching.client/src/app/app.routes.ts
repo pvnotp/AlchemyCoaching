aba0121b332043e44edc419b0be44068f745064b
Alison <alisonjoyforster@gmail.com>
2026-05-01 14:32:03 -0400

    Initializing About component

--- alchemycoaching.client/src/app/app.routes.ts
+++ alchemycoaching.client/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-
-
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }
 ]
