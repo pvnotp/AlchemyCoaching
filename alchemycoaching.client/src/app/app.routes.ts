e8147fae7f80d35161343b76f685c467e999746a
Alison <alisonjoyforster@gmail.com>
2026-05-03 16:58:17 -0400

    Initializaing schedule component

--- alchemycoaching.client/src/app/app.routes.ts
+++ alchemycoaching.client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'schedule', component: ScheduleComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }
 ]
